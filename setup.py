9496b22f4b71ea88a12b26297ac4bf935a3c9f67
kreopt <kreopt@specforge.com>
2017-03-13 09:44:46 +0700

    * update setup script

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@
 
 from distutils.core import setup
 
+from setuptools import find_packages
+
 setup(name='aioweb',
       version='0.1',
       description='asyncronous web framework',
       author='kreopt',
       author_email='kreopt@gmail.com',
       url='https://github.com/kreopt/aioweb/',
+      packages=find_packages(),
      )
