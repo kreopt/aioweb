e8eed5373404e8fd1cf17662f36bb9c55e865982
Platon <platonny@ngs.ru>
2017-04-14 21:47:40 +0700

    wyrm destroy migration

---
+++ wyrm/modules/destroy/migration.py
@@ -0,0 +1,29 @@
+import sys
+import os
+
+brief="delete the migration"
+def usage(argv0):
+    print("Usage: {} destroy migration [MODEL_NAME] MIGRATION_NAME".format(argv0))
+    sys.exit(1)
+
+aliases=['mg']
+def execute(argv, argv0, engine):
+    import lib
+    os.environ.setdefault("AIOWEB_SETTINGS_MODULE", "settings")
+    from aioweb import settings
+    migrations_dir = os.path.abspath( os.path.join(settings.BASE_DIR, 'db/migrations') )
+
+    sys.path.append( os.getcwd() )
+    if len(argv) == 0 or '-h' in argv or '--help' in argv:
+        usage(argv0)
+
+    migration_name = argv[ 0 if len(argv) == 1 else 1 ]
+
+    try: migrations = [os.path.join(migrations_dir, f) for f in os.listdir( migrations_dir ) if migration_name in f]
+    except FileNotFoundError: migrations=[]
+    for m in migrations:
+        print("delete " + m )
+        os.unlink( m )
+
+
+
