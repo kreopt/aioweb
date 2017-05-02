4c5fa4ac15ed35c7fee72d40b9546b7848bf0fc4
kreopt <kreopt@gmail.com>
2017-05-02 09:42:31 +0700

    * auth.user: hash password on create

--- aioweb/contrib/auth/models/user.py
+++ aioweb/contrib/auth/models/user.py
@@ -12,10 +12,19 @@ class User(Model, AbstractUser):
 
     __guarded__ = ['id']
 
+    def __init__(self, _attributes=None, **attributes):
+        if 'password' in attributes:
+            attributes['password'] = User.hash_password(attributes['password'])
+        super().__init__(_attributes, **attributes)
+
+    @staticmethod
+    def hash_password(value):
+        return sha256_crypt.hash(value)
+
     @mutator
     def password(self, value):
         #TODO: configurable hash type
-        self.set_raw_attribute('password', sha256_crypt.hash(value))
+        self.set_raw_attribute('password', User.hash_password(value))
 
     def can(self, permission):
 
