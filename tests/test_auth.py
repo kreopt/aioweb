0ec41cf8b67efba33044d44a69285d7b056f747e
kreopt <kreopt@gmail.com>
2017-05-02 14:49:38 +0700

    * test base: async test wrapper

---
+++ tests/test_auth.py
@@ -0,0 +1,19 @@
+import unittest
+
+import wyrm.base_test
+
+
+class test_Auth(wyrm.base_test.AioWebTestCase):
+
+    async def test_create_user(self):
+        pass
+
+    async def test_auth(self):
+        pass
+
+    async def test_with_one_format(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
--- wyrm/base_test.py
+++ wyrm/base_test.py
@@ -1,15 +1,29 @@
-import unittest, wyrm.lib, os
+import unittest
+import wyrm.lib
+import os
+
+import asyncio
+
+from aioweb.util import awaitable
 from orator.migrations import Migrator, DatabaseMigrationRepository
 from orator import Model
 
 settings = None
 
 
-class AoiWebTestCase(unittest.TestCase):
+def async_test(fn):
+    def decorated(self, *args, **kwargs):
+        self._loop.run_until_complete(awaitable(fn(self, *args, **kwargs)))
+    return decorated
+
+
+class AioWebTestCase(unittest.TestCase):
     refresh_db_before_test = True
 
     def __init__(self, wtf):
         super().__init__(wtf)
+        self._loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(None)
 
     def refresh_db(self):
         resolver = Model.get_connection_resolver()
