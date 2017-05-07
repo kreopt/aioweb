4b507e0d0f86025a96e9f38b3168e1ba760217aa
Your Name <root@example.com>
2017-05-07 14:53:58 +0700

    installing views

--- generators/setup.py
+++ generators/setup.py
@@ -8,9 +8,9 @@ here = os.path.abspath(os.path.dirname(__file__))
 requirements = ["aioweb"]
 
 views_files=[]
-for root, dirs, files in os.walk("app/views"):
+for root, dirs, files in os.walk("APP_NAME/app/views"):
     if len(files) != 0:
-        views_files.append((root, list(map(lambda f: os.path.join(root, f), files))))
+        views_files += list(map(lambda f: os.path.join(root, f).replace("APP_NAME/", "", 1), files))
 
 setup(name='APP_NAME',
       version='0.1',
@@ -21,5 +21,5 @@ setup(name='APP_NAME',
       packages=find_packages(exclude=('test_project', 'tests')),
       include_package_data=True,
       install_requires=requirements,
-      package_data= {'aioweb': views_files},
+      package_data= {'APP_NAME': views_files},
       )
