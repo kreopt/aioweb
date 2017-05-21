a205e59787c2d1109769d47dc1179bd849fd64a5
kreopt <kreopt@gmail.com>
2017-05-21 22:15:58 +0700

    * revert setup.py template

--- generators/setup.py
+++ generators/setup.py
@@ -8,11 +8,11 @@ here = os.path.abspath(os.path.dirname(__file__))
 requirements = ["aioweb"]
 
 views_files=[]
-for root, dirs, files in os.walk("aioweb_APP_NAME/views"):
+for root, dirs, files in os.walk("APP_NAME/app/views"):
     if len(files) != 0:
-        views_files += list(map(lambda f: os.path.join(root, f).replace("aioweb_APP_NAME/", "", 1), files))
+        views_files += list(map(lambda f: os.path.join(root, f).replace("APP_NAME/", "", 1), files))
 
-setup(name='aioweb_APP_NAME',
+setup(name='APP_NAME',
       version='0.1',
       description='aioweb module',
       author='human being',
@@ -21,5 +21,5 @@ setup(name='aioweb_APP_NAME',
       packages=find_packages(exclude=('test_project', 'tests')),
       include_package_data=True,
       install_requires=requirements,
-      package_data= {'aioweb_APP_NAME': views_files},
+      package_data= {'APP_NAME': views_files},
       )
