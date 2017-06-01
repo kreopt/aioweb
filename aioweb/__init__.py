26e88c7f22da46a3e2465caee6cef5547b5c77a0
kreopt <kreopt@gmail.com>
2017-06-01 14:55:32 +0700

    * run_app multiple servers support * change socket numeration format

--- aioweb/__init__.py
+++ aioweb/__init__.py
@@ -53,7 +53,7 @@ class Application(AioApp):
 def run_app(app, *,
             shutdown_timeout=60.0, ssl_context=None,
             print=print, backlog=128, access_log_format=None,
-            access_log=access_logger):
+            access_log=access_logger, servers=1):
     """Run an app locally"""
     app['env'] = os.environ.get('AIOWEB_ENV', 'development')
     conf = {
@@ -83,16 +83,35 @@ def run_app(app, *,
 
     loop.run_until_complete(app.startup())
     if unix_socket:
-        try:
-            os.unlink(unix_socket)
-        except:
-            pass
-        srv = loop.run_until_complete(loop.create_unix_server(handler, path=unix_socket,
-                                                              ssl=ssl_context,
-                                                              backlog=backlog))
-        os.chmod(unix_socket, 0o664)
-        print("======== Running on unix://{} ========\n"
-              "(Press CTRL+C to quit)".format(unix_socket))
+        sockets = []
+        if servers == 1:
+            sockets = [unix_socket]
+        else:
+            path, ext = os.path.splitext(unix_socket)
+            for i in range(1, servers + 1):
+                sockets.append("{}.{:02d}{}".format(path, i, ext))
+        for socket in sockets:
+            try:
+                os.unlink(socket)
+            except:
+                pass
+            srv = loop.run_until_complete(loop.create_unix_server(handler, path=socket,
+                                                                  ssl=ssl_context,
+                                                                  backlog=backlog))
+            os.chmod(socket, 0o664)
+            print("======== Running on unix://{} ========".format(socket))
+        print("\n(Press CTRL+C to quit)")
+
+        # try:
+        #     os.unlink(unix_socket)
+        # except:
+        #     pass
+        # srv = loop.run_until_complete(loop.create_unix_server(handler, path=unix_socket,
+        #                                                       ssl=ssl_context,
+        #                                                       backlog=backlog))
+        # os.chmod(unix_socket, 0o664)
+        # print("======== Running on unix://{} ========\n"
+        #       "(Press CTRL+C to quit)".format(unix_socket))
     else:
         srv = loop.run_until_complete(loop.create_server(handler, host,
                                                          port, ssl=ssl_context,
@@ -116,6 +135,7 @@ def run_app(app, *,
         loop.run_until_complete(app.cleanup())
     loop.close()
 
+
 def start_app(app, *,
               shutdown_timeout=60.0,
               ssl_context=None,
@@ -125,14 +145,14 @@ def start_app(app, *,
               servers=1,
               log="log/server.log",
               log_level="debug",
-              print=print, 
-              backlog=128, 
+              print=print,
+              backlog=128,
               access_log_format=None,
               access_log=access_logger):
-
     loop = app.loop
     app['env'] = os.environ.get('AIOWEB_ENV', 'development')
-    if not port: port = 8000 if not ssl_context  else 8443
+    if not port:
+        port = 8000 if not ssl_context else 8443
 
     make_handler_kwargs = dict()
     if access_log_format is not None:
@@ -143,13 +163,16 @@ def start_app(app, *,
     if socket:
         sockets = []
         if servers == 1:
-            sockets=[socket]
+            sockets = [socket]
         else:
-            for i in range(1,servers + 1):
-                sockets.append("{:02d}_{}".format(i, socket))
+            path, ext = os.path.splitext(socket)
+            for i in range(1, servers + 1):
+                sockets.append("{}.{:02d}{}".format(path, i, ext))
         for socket in sockets:
-            try: os.unlink(socket)
-            except: pass
+            try:
+                os.unlink(socket)
+            except:
+                pass
             srv = loop.run_until_complete(loop.create_unix_server(handler, path=socket,
                                                                   ssl=ssl_context,
                                                                   backlog=backlog))
--- wyrm/modules/start.py
+++ wyrm/modules/start.py
@@ -5,9 +5,13 @@ from signal import SIGTERM
 brief = "start the aioweb server"
 aliases = ["s", "runserver"]
 
+
 def usage(argv0):
-    print("Usage: {} s [-d] [-p|--port N] [-h|--host HOST] [-c|--config YAML] [--pid PID] [-s|--socket SOCKET] [-S|--servers N]".format(argv0))
+    print(
+        "Usage: {} s [-d] [-p|--port N] [-h|--host HOST] [-c|--config YAML] [--pid PID] [-s|--socket SOCKET] [-S|--servers N]".format(
+            argv0))
     sys.exit(1)
+
 
 def execute(argv, argv0, engine):
     os.environ.setdefault("AIOWEB_SETTINGS_MODULE", "settings")
@@ -21,33 +25,36 @@ def execute(argv, argv0, engine):
     from time import sleep
     from aioweb.conf import settings
     from aioweb.util.conf_reader import ConfigReader
-    optlist, args = getopt.getopt(argv, "dhc:p:s:S:h:", ["help", "port=", "config=", "socket=", "servers=", "pid=", "host="])
+    optlist, args = getopt.getopt(argv, "dhc:p:s:S:h:",
+                                  ["help", "port=", "config=", "socket=", "servers=", "pid=", "host="])
 
-    kwargs={}
-    daemonize=False
-    pid_file="aioweb.pid"
+    kwargs = {}
+    daemonize = False
+    pid_file = "aioweb.pid"
     if ('-h', '') in optlist or ('--help', '') in optlist:
         usage(argv0)
 
     config_file = "config/server.yml"
-    for opt,val in optlist:
-        if opt in ['--config', '-c']: config_file = val
+    for opt, val in optlist:
+        if opt in ['--config', '-c']:
+            config_file = val
 
     if os.path.exists(config_file):
-        with open(config_file, "r") as f: conf = yaml.load(f.read())
+        with open(config_file, "r") as f:
+            conf = yaml.load(f.read())
         kwargs.update(conf.get("common", {}))
-        env=os.environ.get('AIOWEB_ENV', 'development')
+        env = os.environ.get('AIOWEB_ENV', 'development')
         kwargs.update(conf.get(env, {}))
 
-    for opt,val in optlist:
+    for opt, val in optlist:
         if opt in ['--port', '-p']:
             kwargs["port"] = int(val)
         elif opt in ['-d']:
-            daemonize=True
+            daemonize = True
         elif opt in ['-h', '--host']:
-            kwargs["host"]=val
+            kwargs["host"] = val
         elif opt in ['-s', '--socket']:
-            kwargs["socket"]=val
+            kwargs["socket"] = val
         elif opt in ['-pid']:
             pid_file = val
         elif opt in ['-S', '--servers']:
@@ -69,13 +76,15 @@ def execute(argv, argv0, engine):
 
     if daemonize:
         if os.path.exists(pid_file):
-            with open(pid_file, "r") as f: pid=int(f.read())
+            with open(pid_file, "r") as f:
+                pid = int(f.read())
             print("killing process( PID: {} )".format(pid))
             try:
                 os.kill(pid, SIGTERM)
-                for i in range(0,10):
+                for i in range(0, 10):
                     sleep(1)
-                    if not os.path.exists(pid_file): break
+                    if not os.path.exists(pid_file):
+                        break
             except ProcessLookupError:
                 os.unlink(pid_file)
 
