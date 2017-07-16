0caab0a4ef56cdfc7ad5b069c7dfb93abc9a5d6e
kreopt <kreopt@gmail.com>
2017-07-16 13:47:10 +0700

    wyrm: specify table name instead of model name in migration creator

--- wyrm/modules/generate/migration.py
+++ wyrm/modules/generate/migration.py
@@ -6,7 +6,7 @@ brief = "create a new migration"
 
 def usage(argv0):
     print(
-        "Usage: {} generate migration MODEL_NAME MIGRATION_NAME [FIELD_NAME:FIELD_TYPE] [FIELD_NAME:FIELD_TYPE] [...]".format(
+        "Usage: {} generate migration TABLE_NAME MIGRATION_NAME [FIELD_NAME:FIELD_TYPE] [FIELD_NAME:FIELD_TYPE] [...]".format(
             argv0))
     sys.exit(1)
 
@@ -26,26 +26,21 @@ def execute(argv, argv0, engine):
 
     additional_fields = lib.get_fields_from_argv(argv[2:], usage, argv0)
 
-    migrations_dir, models_dir = lib.dirs(settings, format=["migrations", "models"])
-    table, model_name, model_class = lib.names(argv[0], ["table", "model", "class"])
-
+    migrations_dir = lib.dirs(settings, format=["migrations"])
+    
+    table_name = argv[0]
     migration_name = argv[1]
-    model_file = os.path.join(models_dir, "{}.py".format(model_name))
-
-    if not os.path.exists(model_file):
-        if lib.ask("{} does not exists.\nDo you wanna continue?".format(model_file)) == 'n':
-            print("migration aborted")
-            sys.exit(1)
-
+   
     try:
         migrations = [os.path.join(migrations_dir, f) for f in os.listdir(migrations_dir) if migration_name in f]
     except FileNotFoundError:
         migrations = []
     rc = None
     for m in migrations:
-        if rc not in ['a', 'all']: rc = lib.ask(
-            "\n{} already exists.\nDo you wanna remove it before create migration?".format(m),
-            ['y', 'n', 'e', 'exit', 'all', 'a', 'skip all'])
+        if rc not in ['a', 'all']: 
+            rc = lib.ask(
+                "\n{} already exists.\nDo you wanna remove it before create migration?".format(m),
+                ['y', 'n', 'e', 'exit', 'all', 'a', 'skip all'])
         if rc in ['skip all']:
             break
         if rc in ['e', 'exit']:
@@ -56,15 +51,10 @@ def execute(argv, argv0, engine):
             print("delete " + m)
             os.unlink(m)
 
-    os.system("orator make:migration {} -p {} -t {}".format(migration_name, migrations_dir, table))
+    os.system("orator make:migration {} -p {} -t {}".format(migration_name, migrations_dir, table_name))
     file_name = os.path.join(migrations_dir, [f for f in sorted(os.listdir(migrations_dir)) if migration_name in f][-1])
     print("patching " + file_name)
     lib.insert_in_python(file_name, ["def up", "as table:"],
-                         ["table.{}('{}').nullable()".format(tp, name) for tp, name in additional_fields])
+                         ["table.{}('{}')".format(tp, name) for tp, name in additional_fields])
     lib.insert_in_python(file_name, ["def down", "as table:"],
-                         ["table.drop_column('{}')".format(name) for tp, name in additional_fields])
-    if os.path.exists(model_file):
-        print("patching " + model_file)
-        lib.insert_in_python(model_file, ["class"],
-                             ["# {}{} - {}".format(name, (25 - len(name)) * ' ', tp) for tp, name in additional_fields],
-                             ignore_pass=True)
+                         ["table.drop_column('{}')".format(name) for tp, name in additional_fields])   
